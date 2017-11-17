d6d8eee29cc30912d691241d2ecdf0952b67ae5d
Mijail Hernandez <33376402+MishaHernandez@users.noreply.github.com>
2017-11-17 13:27:52 -0500

    Add files via upload

---
+++ src/version4/Conector.java
@@ -0,0 +1,37 @@
+/*
+ * conexion con base de datos SQLite
+ */
+package version4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector{
+    Connection conexion;
+    String ruta;
+
+    public void conectar(){ //abrir la conexion
+        try{
+            ruta = "caja_piura.db"; //dentro del mismo proyecto
+            conexion = DriverManager.getConnection("jdbc:sqlite:" +ruta);
+            
+            if(conexion!=null){ //mensaje de conexion exitosa
+                //JOptionPane.showMessageDialog(null, "Conectado");
+                System.out.println("Conectado...");
+            }
+        }
+        catch(SQLException e){
+            System.err.println("No hay conexion: "+e);
+        }
+    }
+    
+    public void cerrar(){ //cerrar conexion con la bd
+        try {
+            conexion.close();
+        } catch (SQLException e) {
+            System.err.println("Error de cierre"+e);
+        }
+    }
+    
+}
---
+++ src/version4/Operaciones.java
@@ -0,0 +1,244 @@
+package version4;
+/*
+*Aqui se guardan todas la funciones que utilizan sentencias sql
+*se guarda la transaccion y se actualiza el saldo de cada cliente
+*/
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import javax.swing.JOptionPane;
+
+public class Operaciones {
+    Conector con;
+    Voraz voraz;
+
+    public Operaciones() {
+        con = new Conector();
+        voraz = new Voraz();
+    }
+    
+    //===OPERACIONES PRINCIPALES===
+    public void depositar(){
+        int nroCta, monto,saldo;
+        String tipo = "deposito";
+        String sql, fecha;
+        ResultSet result, result1;
+        
+        con.conectar(); //abre conexion
+        
+        nroCta=Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese Nro de Cta: "));
+        try{
+            PreparedStatement st = con.conexion.prepareStatement("select * from cuenta where idcuenta = "+nroCta+"");
+            result = st.executeQuery(); //guarda los datos de la cuenta
+            saldo=result.getInt("saldo"); //obtiene el saldo de la tabla "cuenta"
+            
+            if(result.getInt("idcuenta") == nroCta){
+                //validar operaciones del día -> obtener suma de depositos
+                fecha = getFecha();
+                sql="select sum(monto) from transaccion where idcuenta = "+nroCta+" and tipo = 'deposito' and fecha = '"+fecha+"'";
+                PreparedStatement st1 = con.conexion.prepareStatement(sql);
+                result1 = st1.executeQuery(); //obtiene el historial de depositos
+                //validarOperacion(result1); //valida si la cuenta está habilitada para depositos
+                
+                monto=Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese el Monto: "));
+                
+                //validarMontoDeposito(monto, result1); //valida el monto a depositar
+                guardar_Transaccion(monto,nroCta,tipo); //guarda la transaccion en la tabla "transaccion"
+                actualizar_Deposito(monto,nroCta,saldo); //actualiza el saldo de la tabla "cuenta"
+                
+                JOptionPane.showMessageDialog(null, "Operación realizada con éxito");
+                System.out.println("Se ha depositado: "+monto);
+                System.out.println(" ");
+                
+                //voraz.vuelto(monto); //devuelve el cambio
+
+                con.cerrar(); //cierra conexion
+            }
+        }
+        catch(SQLException e){
+            System.err.println("error en deposito: "+e);
+        }
+    }
+    
+    public void retirar(){
+        int nroCta, monto,saldo;
+        String tipo = "retiro";
+        String sql, fecha;
+        ResultSet result, result1;
+        
+        con.conectar(); //abre conexion
+        
+        nroCta=Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese Nro de Cta: "));
+        try{
+            PreparedStatement st = con.conexion.prepareStatement("select * from cuenta where idcuenta = "+nroCta+"");
+            result = st.executeQuery(); //obtiene los datos de la cuent
+            saldo=result.getInt("saldo"); //obtiene el saldo de la tabla "cuenta"
+            
+            if(result.getInt("idcuenta") == nroCta){
+                //validar operaciones del día -> obtener suma de retiros
+                fecha = getFecha();
+                sql="select sum(monto) from transaccion where idcuenta = "+nroCta+" and tipo = 'retiro' and fecha = '"+fecha+"'";
+                PreparedStatement st1 = con.conexion.prepareStatement(sql);
+                result1 = st1.executeQuery(); //obtiene el historial de retiros del día
+                validarOperacion(result1); //valida si la cuenta está habilitada para retiros
+                
+                monto=Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese Monto: "));
+                
+                validarMontoRetiro(monto, result1, saldo); //valida el monto a retirar
+                guardar_Transaccion(monto,nroCta,tipo); //guarda la transaccion en la tabla "transaccion"
+                actualizar_Retiro(monto,nroCta,saldo); //actualiza el saldo de la tabla "cuenta"
+                
+                JOptionPane.showMessageDialog(null, "Operación realizada con éxito");
+                System.out.println("Se ha retirado: "+monto);
+                System.out.println(" ");
+                
+                voraz.vuelto(monto); //devuelve el cambio
+                
+                con.cerrar(); //cierra conexion
+            }
+        }
+        catch(SQLException e){
+            System.err.println("error en retiro: "+e);
+        }
+    }
+    
+    public void infoSaldo(){
+        int nroCta;
+        ResultSet result;
+        
+        con.conectar(); //abre conexion
+        
+        nroCta=Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese Nro de Cta: "));
+        try{
+            PreparedStatement st = con.conexion.prepareStatement("select * from cuenta where idcuenta = "+nroCta+"");
+            result = st.executeQuery(); //ejecutamos la consulta y obtenemos el resultado
+            
+            if(result.getInt("idcuenta") == nroCta){
+                System.out.println("Su saldo es de: "+result.getInt("saldo") +" "+ result.getString("divisa"));
+                
+                con.cerrar(); //cierra conexion
+            }
+        }
+        catch(SQLException e){
+            System.err.println("Error en informacion: "+e);
+        }
+    }
+    
+    //===OPERACIONES SECUNDARIAS===
+    public void guardar_Transaccion(int monto, int nroCta, String tipo){
+        String fecha = getFecha();
+        int nrotransac = getNroTransac();
+        String sql="insert into transaccion (idtransac, idcuenta, tipo, monto, fecha) values (?,?,?,?,?)";
+        
+        try {
+            PreparedStatement st = con.conexion.prepareStatement(sql);
+            
+            st.setInt(1, nrotransac);
+            st.setInt(2, nroCta);
+            st.setString(3, tipo);
+            st.setInt(4, monto);
+            st.setString(5, fecha);
+            st.execute(); //ejecutar consulta
+            
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public void actualizar_Retiro(int monto, int nroCta, int saldo){
+        int actual = saldo - monto; //restamos el valor retirado
+        String sql = "update cuenta set saldo = ? where idcuenta = ?";
+        
+        try {
+            PreparedStatement st = con.conexion.prepareStatement(sql);
+            
+            st.setInt(1, actual);
+            st.setInt(2, nroCta);
+            st.executeUpdate(); //ejecutar consulta
+            
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public void actualizar_Deposito(int monto, int nroCta, int saldo){
+        int actual = saldo + monto; //sumamos el valor depositado
+        String sql = "update cuenta set saldo = "+actual+" where idcuenta = "+nroCta+"";
+        
+        try {
+            PreparedStatement st = con.conexion.prepareStatement(sql);
+            
+            st.executeUpdate(); //ejecutar consulta
+            
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public void validarOperacion(ResultSet result1) throws SQLException{ //valida el efectivo del día
+        int total;
+        
+        total=result1.getInt(1);
+        if(total==3000){
+            JOptionPane.showMessageDialog(null, "Ha sobrepasado el límite de efectivo que puede mover por día");
+            System.exit(0);
+        }
+    }
+    
+    public void validarMontoDeposito(int monto, ResultSet result1) throws SQLException{
+        int historial;
+        
+        historial=result1.getInt(1);
+        
+        if(monto<10 || monto>3000){
+            JOptionPane.showMessageDialog(null, "Debe hacer un deposito no mayor a 3mil ni menor a 10");
+            System.exit(0);
+        }
+        else if((monto+historial)>3000){
+            JOptionPane.showMessageDialog(null, "Solo puede hacer un deposito no mayor a "+(3000-historial));
+            System.exit(0);
+        }
+    }
+    
+    public void validarMontoRetiro(int monto, ResultSet result1, int saldo) throws SQLException{
+        int historial;
+        
+        historial=result1.getInt(1);
+        
+        if(saldo >= monto){ //Si retiro no excede el saldo
+            if(monto<10 || monto>3000){ //si retiro no está entre 10 y 3mil
+                JOptionPane.showMessageDialog(null, "Debe hacer un retiro no mayor a 3mil ni menor a 10");
+                System.exit(0);
+            }
+            else if((monto+historial)>3000){ //si retiros del día suman más de 3mil
+                JOptionPane.showMessageDialog(null, "Solo puede hacer un retiro no mayor a "+(3000-historial));
+                System.exit(0);
+            }
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "-El saldo es insuficiente\n"+"-El saldo actual es de "+saldo);
+            System.exit(0);
+        }
+    }
+    
+    public String getFecha(){ //obtiene la fecha del sistema
+        Calendar fecha = new GregorianCalendar();
+        
+        String formato;
+        int año = fecha.get(Calendar.YEAR);
+        int mes = fecha.get(Calendar.MONTH);
+        int dia = fecha.get(Calendar.DAY_OF_MONTH);
+        
+        formato = dia + "/" + (mes+1) + "/" + año; //formato DD/MM/AAAA
+        
+        return formato;
+    }
+    
+    public int getNroTransac(){ //genera un codigo de la transaccion
+        int clave = (int) (Math.random() * 2000 + 1000); //numero aleatorio de 4 cifras
+        
+        return clave;
+    }
+}
